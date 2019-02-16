057ada66e61ae28f9f0c3aba37f77c6719172cb8
Elvis de Freitas <edigitalb@gmail.com>
2019-02-16 18:42:40 -0200

    setup files upload example

---
+++ resteasy-examples/src/main/java/com/mageddo/imgur/ImageUploadRes.java
@@ -0,0 +1,15 @@
+package com.mageddo.imgur;
+
+public class ImageUploadRes {
+
+	private String link;
+
+	public String getLink() {
+		return link;
+	}
+
+	public ImageUploadRes setLink(String link) {
+		this.link = link;
+		return this;
+	}
+}
