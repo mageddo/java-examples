bdb3e1f6c1b50c98fcbefc98a55fd582b2709cef
Elvis de Freitas <edigitalb@gmail.com>
2019-02-26 02:05:47 -0300

    setup example

--- annotation-processor/example/src/main/java/MutableClass.java
+++ annotation-processor/example/src/main/java/MutableClass.java
@@ -2,12 +2,13 @@ import com.mageddo.processor.Immutable;
 
 @Immutable
 public class MutableClass {
+
     private String name;
-    
+
     public MutableClass( final String name ) {
         this.name = name;
     }
-    
+
     public String getName() {
         return name;
     }
