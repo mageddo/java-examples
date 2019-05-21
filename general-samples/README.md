9fd736e2bc9af83ccdf95b06837c6c69cd4a9f21
elfreitas <edigitalb@gmail.com>
2019-05-21 12:28:45 -0300

    removing commented code

--- general-samples/README.md
+++ general-samples/README.md
@@ -1,3 +1,6 @@
-### Benchmark
 
 	./gradlew general-samples:jmh
+
+
+### Links
+* [Examples](https://github.com/melix/jmh-gradle-example/tree/master/src/jmh/java/org/openjdk/jmh/samples)
--- general-samples/src/jmh/java/com/mageddo/benchmark/JMHSample_02_BenchmarkModes.java
+++ general-samples/src/jmh/java/com/mageddo/benchmark/JMHSample_02_BenchmarkModes.java
@@ -9,6 +9,7 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class JMHSample_02_BenchmarkModes {
@@ -45,99 +46,10 @@ public class JMHSample_02_BenchmarkModes {
 
     @Benchmark()
     @BenchmarkMode(Mode.SampleTime)
-    @OutputTimeUnit(TimeUnit.SECONDS)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
     public void measureThroughput() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(100);
+        TimeUnit.MILLISECONDS.sleep(new Random().nextInt(150));
     }
-
-//    /*
-//     * Mode.AverageTime measures the average execution time, and it does it
-//     * in the way similar to Mode.Throughput.
-//     *
-//     * Some might say it is the reciprocal throughput, and it really is.
-//     * There are workloads where measuring times is more convenient though.
-//     */
-//
-//    @Benchmark
-//    @BenchmarkMode(Mode.AverageTime)
-//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-//    public void measureAvgTime() throws InterruptedException {
-//        TimeUnit.MILLISECONDS.sleep(100);
-//    }
-//
-//    /*
-//     * Mode.SampleTime samples the execution time. With this mode, we are
-//     * still running the method in a time-bound iteration, but instead of
-//     * measuring the total time, we measure the time spent in *some* of
-//     * the benchmark method calls.
-//     *
-//     * This allows us to infer the distributions, percentiles, etc.
-//     *
-//     * JMH also tries to auto-adjust sampling frequency: if the method
-//     * is long enough, you will end up capturing all the samples.
-//     */
-//    @Benchmark
-//    @BenchmarkMode(Mode.SampleTime)
-//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-//    public void measureSamples() throws InterruptedException {
-//        TimeUnit.MILLISECONDS.sleep(100);
-//    }
-//
-//    /*
-//     * Mode.SingleShotTime measures the single method invocation time. As the Javadoc
-//     * suggests, we do only the single benchmark method invocation. The iteration
-//     * time is meaningless in this mode: as soon as benchmark method stops, the
-//     * iteration is over.
-//     *
-//     * This mode is useful to do cold startup tests, when you specifically
-//     * do not want to call the benchmark method continuously.
-//     */
-//    @Benchmark
-//    @BenchmarkMode(Mode.SingleShotTime)
-//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-//    public void measureSingleShot() throws InterruptedException {
-//        TimeUnit.MILLISECONDS.sleep(100);
-//    }
-//
-//    /*
-//     * We can also ask for multiple benchmark modes at once. All the tests
-//     * above can be replaced with just a single test like this:
-//     */
-//    @Benchmark
-//    @BenchmarkMode({Mode.Throughput, Mode.AverageTime, Mode.SampleTime, Mode.SingleShotTime})
-//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-//    public void measureMultiple() throws InterruptedException {
-//        TimeUnit.MILLISECONDS.sleep(100);
-//    }
-//
-//    /*
-//     * Or even...
-//     */
-//
-//    @Benchmark
-//    @BenchmarkMode(Mode.All)
-//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
-//    public void measureAll() throws InterruptedException {
-//        TimeUnit.MILLISECONDS.sleep(100);
-//    }
-
-    /*
-     * ============================== HOW TO RUN THIS TEST: ====================================
-     *
-     * You are expected to see the different run modes for the same benchmark.
-     * Note the units are different, scores are consistent with each other.
-     *
-     * You can run this test:
-     *
-     * a) Via the command line:
-     *    $ mvn clean install
-     *    $ java -jar target/benchmarks.jar JMHSample_02 -f 1
-     *    (we requested a single fork; there are also other options, see -h)
-     *
-     * b) Via the Java API:
-     *    (see the JMH homepage for possible caveats when running from IDE:
-     *      http://openjdk.java.net/projects/code-tools/jmh/)
-     */
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
