ba53b4f067fb60b0cdc616fd0107f75cc11181ed
Elvis de Freitas <edigitalb@gmail.com>
2019-07-27 01:30:21 -0300

    setup avro serializers

---
+++ avro-examples/src/main/java/com/mageddo/avro/AvroUtils.java
@@ -0,0 +1,47 @@
+package com.mageddo.avro;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericContainer;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+
+public class AvroUtils {
+
+	public static byte[] serialize(GenericContainer o){
+
+		final var writer = new GenericDatumWriter<>(o.getSchema());
+		final var bout = new ByteArrayOutputStream();
+		try {
+			final var encoder = EncoderFactory
+				.get()
+				.binaryEncoder(bout, null);
+			writer.write(o, encoder);
+			encoder.flush();
+			return bout.toByteArray();
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+
+	}
+
+	public static <T>T deserialize(byte[] data, Schema schema){
+		// Deserialize Users from disk
+		final var reader = new SpecificDatumReader<>(schema);
+		try {
+			return (T) reader.read(
+				null,
+				DecoderFactory
+					.get()
+					.binaryDecoder(data, null)
+			);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+}
---
+++ avro-examples/src/test/java/com/mageddo/avro/AvroUtilsTest.java
@@ -0,0 +1,50 @@
+package com.mageddo.avro;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class AvroUtilsTest {
+
+	@Test
+	public void serialize() {
+
+		// arrange
+		User user = User
+			.newBuilder()
+			.setName("Elvis")
+			.setFavoriteColor("Green")
+			.setFavoriteNumber(7)
+			.build();
+
+		// act
+		final byte[] avroBytes = AvroUtils.serialize(user);
+
+		// assert
+		assertNotNull(avroBytes);
+	}
+
+	@Test
+	public void deserialize() throws Exception {
+
+		// arrange
+		byte[] avroBytes = getResourceAsBytes("/avro-utils-test/001.avro");
+
+		// act
+		final User user = AvroUtils.deserialize(avroBytes, User.getClassSchema());
+
+		// assert
+		assertEquals("Elvis", user.getName());
+		assertEquals(Integer.valueOf(7), user.getFavoriteNumber());
+		assertEquals("Green", user.getFavoriteColor());
+
+	}
+
+	private byte[] getResourceAsBytes(String path) throws IOException {
+		return IOUtils.toByteArray(getClass().getResourceAsStream(path));
+	}
+}
