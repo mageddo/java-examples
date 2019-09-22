b7ea78b8366bc0d1234c5fd61c0b43262a873fa5
Elvis de Freitas <edigitalb@gmail.com>
2019-09-22 15:31:41 -0300

    upgrading

--- thymeleaf-examples/src/test/java/com/mageddo/thymeleaf/TemplateUtils.java
+++ thymeleaf-examples/src/test/java/com/mageddo/thymeleaf/TemplateUtils.java
@@ -1,4 +1,4 @@
-package com.mageddo.thymeleaf;
+package com.mageddo.utils;
 
 import org.apache.commons.io.IOUtils;
 import org.thymeleaf.TemplateEngine;
@@ -18,29 +18,32 @@ public final class TemplateUtils {
 	private TemplateUtils() {
 	}
 
+	public static String processFromPath(String templateName, Map<String, Object> variables) {
+		return processFromPath(templateName, new Context(Locale.getDefault(), variables));
+	}
+
+	public static String processFromPath(String templateName, Context context) {
+		try {
+			return process(IOUtils.toString(
+				TemplateUtils.class.getResourceAsStream(templateName),
+				Charset.defaultCharset()
+			), context);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
 	public static String process(String templateName, Map<String, Object> variables) {
 		return process(templateName, new Context(Locale.getDefault(), variables));
 	}
 
-	public static String process(String templateName, Context context) {
-
-		if(templateName.startsWith("/")){
-			try {
-				templateName = IOUtils.toString(
-					TemplateUtils.class.getResourceAsStream(templateName),
-					Charset.defaultCharset()
-				);
-			} catch (IOException e) {
-				throw new UncheckedIOException(e);
-			}
-		}
-
+	public static String process(String template, Context context){
 		TemplateEngine templateEngine = new TemplateEngine();
 		StringTemplateResolver templateResolver = new StringTemplateResolver();
 		templateResolver.setTemplateMode(TemplateMode.HTML);
 		templateEngine.setTemplateResolver(templateResolver);
 		StringWriter stringWriter = new StringWriter();
-		templateEngine.process(templateName, context, stringWriter);
+		templateEngine.process(template, context, stringWriter);
 		return stringWriter.toString();
 	}
 }
--- thymeleaf-examples/src/test/java/com/mageddo/thymeleaf/TemplateUtilsTest.java
+++ thymeleaf-examples/src/test/java/com/mageddo/thymeleaf/TemplateUtilsTest.java
@@ -1,5 +1,6 @@
 package com.mageddo.thymeleaf;
 
+import com.mageddo.utils.TemplateUtils;
 import org.junit.jupiter.api.Test;
 import org.thymeleaf.context.Context;
 
@@ -13,7 +14,7 @@ public class TemplateUtilsTest {
 		Context context = new Context();
 		context.setVariable("name", "World");
 
-		String out = TemplateUtils.process("/templates/index.html", context);
+		String out = TemplateUtils.processFromPath("/templates/index.html", context);
 
 		assertTrue(out.contains("<p>World</p>"));
 	}
