80ed9e9aed7c90e376a9d504512f8b9b0a26d1fd
Elvis de Freitas <edigitalb@gmail.com>
2019-10-26 12:59:22 -0300

    setup more details

--- distributed-tracing/zipkin-examples/src/main/java/Main.java
+++ distributed-tracing/zipkin-examples/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import brave.Tracing;
+import brave.propagation.TraceContext;
 import zipkin2.reporter.AsyncReporter;
 import zipkin2.reporter.okhttp3.OkHttpSender;
 
@@ -19,39 +20,76 @@ public class Main {
 			.build();
 
 		// Create a tracing component with the service name you want to see in Zipkin.
-		var tracing = Tracing.newBuilder()
+		Tracing.newBuilder()
 			.localServiceName("bitskins")
 			.spanReporter(spanReporter)
 			.build();
 
-		var tracer = tracing.tracer();
-		tracer.withSpanInScope(tracer.nextSpan().name("bot-skin-buyer").start());
-		tracer.currentSpan().finish();
+		var tracer = Tracing.currentTracer();
+		tracer.startScopedSpan("bot-skin-buyer");
+		tracer
+		.currentSpan()
+		.tag("skinId", "14895514112BSL543100268")
+		.finish();
 
-		tracer.withSpanInScope(tracer.nextSpan().name("find-skins-to-buy"));
-		tracer.currentSpan().tag("x", "y");
+		tracer.withSpanInScope(tracer.nextSpan().name("find-skins-to-buy").start());
+		tracer
+		.currentSpan()
+		.tag("x", "y")
+		.tag("skinId", "14895514112BSL543100268")
+		;
 		Thread.sleep(300);
 		tracer.currentSpan().finish();
 
-		tracer.withSpanInScope(tracer.nextSpan().name("calculate-profit"));
-		tracer.currentSpan().tag("x", "y");
+		tracer.startScopedSpan("calculate-profit");
+		tracer
+		.currentSpan()
+		.tag("x", "y")
+		.tag("skinId", "14895514112BSL543100268")
+		;
 		Thread.sleep(50);
 		tracer.currentSpan().finish();
 
-		tracer.withSpanInScope(tracer.nextSpan().name("check-balance-to-buy"));
-		tracer.currentSpan().tag("x", "y");
+		tracer.startScopedSpan("check-balance-to-buy");
+		tracer
+		.currentSpan()
+		.tag("x", "y")
+		.tag("skinId", "14895514112BSL543100268")
+		;
 
 		Thread.sleep(250);
 		tracer.currentSpan().finish();
 
-		tracer.withSpanInScope(tracer.nextSpan().name("send-buy-order"));
-		tracer.currentSpan().tag("x", "y");
+		tracer.startScopedSpan("send-buy-order");
+		tracer
+		.currentSpan()
+		.tag("x", "y")
+		.tag("skinId", "14895514112BSL543100268")
+		;
 		Thread.sleep(30);
 		tracer.currentSpan().finish();
 
+		final var traceContext = Tracing.current().currentTraceContext().get();
+		tracer.withSpanInScope(tracer.newChild(
+			TraceContext
+				.newBuilder()
+				.traceId(traceContext.traceId())
+				.spanId(traceContext.spanId())
+				.build()
+		).name("buy-order-confirmation").start());
+		sleep(321);
+		tracer.currentSpan().finish();
 
-		tracing.close();
+		Tracing.current().close();
 		spanReporter.close();
 		sender.close();
+	}
+
+	public static void sleep(long time){
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
