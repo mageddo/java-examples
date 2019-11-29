5900c88b2b25646b3dcb1c0768657f9e44c0cbea
Elvis de Freitas <edigitalb@gmail.com>
2019-11-29 01:30:34 -0300

    setup codestyle

--- src/main/java/com/mageddo/codestyle/Stuff.java
+++ src/main/java/com/mageddo/codestyle/Stuff.java
@@ -1,25 +1,34 @@
 package com.mageddo.codestyle;
 
+import java.util.List;
+
+import static java.util.Collections.singletonMap;
+
 public class Stuff {
-    public void aVeryLongMethodNameExplainingWhatItDoes(String argumentA, String argumentB,
-                                                        String argumentC, String argumentD, String argumentE, String argumentF
-    ) {
-        System.out.println("do stuff");
+  public void aVeryLongMethodNameExplainingWhatItDoes(String argumentA, String argumentB,
+      String argumentC, String argumentD, String argumentE, String argumentF
+  ) {
+    System.out.println("do stuff");
+  }
+
+  public static void main(String[] args) {
+
+    final List<String> fruits = List.of("apple", "orange");
+
+    final var myObject = singletonMap("key", "value");
+
+    new Stuff().aVeryLongMethodNameExplainingWhatItDoes(
+        "argument A value", "argument B value", "string for argument C", "argument D value",
+        "argument E value", "argument F value"
+    );
+
+    if (1 == 1) {
+
+
     }
 
-    public static void main(String[] args) {
-        new Stuff().aVeryLongMethodNameExplainingWhatItDoes(
-                "argument A value", "argument B value", "string for argument C", "argument D value",
-                "argument E value", "argument F value"
-        );
+    for (int i = 0; i < 10; i++) {
 
-        if (1 == 1) {
-
-
-        }
-
-        for (int i = 0; i < 10; i++) {
-
-        }
     }
+  }
 }
