8cd9f2f65aeb8e18afa605feacfc9401b3b7dc3d
Elvis de Freitas <edigitalb@gmail.com>
2020-03-18 22:17:29 -0300

    setup micronaut openapi with application properties example

--- micronaut-openapi/src/main/java/com/mageddo/micronaut/openapi/controller/FruitController.java
+++ micronaut-openapi/src/main/java/com/mageddo/micronaut/openapi/controller/FruitController.java
@@ -2,15 +2,22 @@ package com.mageddo.micronaut.openapi.controller;
 
 import java.util.List;
 
+import com.mageddo.micronaut.openapi.entity.Fruit;
+
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.swagger.v3.oas.annotations.Operation;
 
 @Controller
 public class FruitController {
+
   @Get
   @Operation(summary = "some action to list fruits")
-  public List<String> fruitList() {
-    return List.of("Apple", "Orange", "Grape");
+  public List<Fruit> fruitList() {
+    return List.of(
+        new Fruit("Apple"),
+        new Fruit("Orange"),
+        new Fruit("Grape")
+    );
   }
 }
