595c570c9c6a8820cf36b43f19a4fa2476f24df6
Elvis de Freitas <edigitalb@gmail.com>
2020-04-27 20:48:43 -0300

    setup shadow jar example

---
+++ gradle-examples/shadow-plugin/src/main/java/com/mageddo/GradleShadowApp.java
@@ -0,0 +1,9 @@
+package com.mageddo;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class GradleShadowApp {
+  public static void main(String args[]){
+    System.out.println(StringUtils.reverse("!!!dlroW olleH"));
+  }
+}
