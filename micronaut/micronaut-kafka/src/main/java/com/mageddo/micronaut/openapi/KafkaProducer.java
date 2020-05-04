5eff37a29580aba1884e2cbe1809928fdace1d16
Elvis de Freitas <edigitalb@gmail.com>
2020-05-04 20:43:35 -0300

    setup micronaut kafka

--- micronaut/micronaut-kafka/src/main/java/com/mageddo/micronaut/openapi/KafkaProducer.java
+++ micronaut/micronaut-kafka/src/main/java/com/mageddo/micronaut/openapi/KafkaProducer.java
@@ -1,23 +1,25 @@
 package com.mageddo.micronaut.openapi;
 
-import io.micronaut.configuration.kafka.annotation.KafkaClient;
+import java.util.concurrent.Future;
+
+import javax.inject.Singleton;
+
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
-import javax.inject.Singleton;
-import java.util.concurrent.Future;
+import io.micronaut.configuration.kafka.annotation.KafkaClient;
 
 @Singleton
 public class KafkaProducer {
 
-	private final Producer<String, byte[]>  producer;
+  private final Producer<String, byte[]> producer;
 
-	public KafkaProducer(@KafkaClient("vanilla") Producer<String, byte[]> producer) {
-		this.producer = producer;
-	}
+  public KafkaProducer(@KafkaClient("vanilla") Producer<String, byte[]> producer) {
+    this.producer = producer;
+  }
 
-	public Future<RecordMetadata> send(ProducerRecord<String, byte[]> record){
-		return producer.send(record);
-	}
+  public Future<RecordMetadata> send(ProducerRecord<String, byte[]> record) {
+    return producer.send(record);
+  }
 }
--- micronaut/micronaut-kafka/src/main/java/com/mageddo/micronaut/openapi/mdb/NewsMDB.java
+++ micronaut/micronaut-kafka/src/main/java/com/mageddo/micronaut/openapi/mdb/NewsMDB.java
@@ -1,34 +1,35 @@
 package com.mageddo.micronaut.openapi.mdb;
 
+import javax.inject.Singleton;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.micronaut.configuration.kafka.annotation.KafkaListener;
 import io.micronaut.configuration.kafka.annotation.OffsetStrategy;
 import io.micronaut.configuration.kafka.annotation.Topic;
 import io.micronaut.configuration.kafka.exceptions.KafkaListenerException;
 import io.micronaut.configuration.kafka.exceptions.KafkaListenerExceptionHandler;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.inject.Singleton;
 
 @Singleton
 @KafkaListener(groupId = "newsGroupId", clientId = "vanilla", offsetStrategy = OffsetStrategy.SYNC)
-public class NewsMDB implements KafkaListenerExceptionHandler  {
+public class NewsMDB implements KafkaListenerExceptionHandler {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+  private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Topic("news")
-	public void receive(ConsumerRecord<String, byte[]> record) {
-		logger.info(
-			"status=ping, key={}, partition={}, offset={}, record={}",
-			record.key(), record.partition(), record.offset(), new String(record.value())
-		);
-	}
+  @Topic("news")
+  public void receive(ConsumerRecord<String, byte[]> record) {
+    logger.info(
+        "status=ping, key={}, partition={}, offset={}, record={}",
+        record.key(), record.partition(), record.offset(), new String(record.value())
+    );
+  }
 
-	@Override
-	public void handle(KafkaListenerException exception) {
-		logger.info("status=custom-exception-handler, msg={}", exception.getMessage());
-	}
+  @Override
+  public void handle(KafkaListenerException exception) {
+    logger.info("status=custom-exception-handler, msg={}", exception.getMessage());
+  }
 
 
 }
