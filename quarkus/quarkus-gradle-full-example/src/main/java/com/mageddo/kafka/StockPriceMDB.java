eca4bc1c3482d28aaa0e9dbf94b456210bfb020b
Elvis de Freitas <edigitalb@gmail.com>
2020-05-08 00:26:21 -0300

    adjusting consumer

--- quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/kafka/StockPriceMDB.java
+++ quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/kafka/StockPriceMDB.java
@@ -1,36 +1,33 @@
 package com.mageddo.kafka;
 
 import java.time.Duration;
-import java.util.Collections;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
 
+import com.mageddo.kafka.client.BatchConsumeCallback;
 import com.mageddo.kafka.client.ConsumerConfig;
 import com.mageddo.kafka.client.Consumers;
+import com.mageddo.kafka.client.RecoverCallback;
 import com.mageddo.kafka.client.RetryPolicy;
 
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
-import io.quarkus.runtime.StartupEvent;
 import io.quarkus.scheduler.Scheduled;
 import io.quarkus.scheduler.ScheduledExecution;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ApplicationScoped
-@RequiredArgsConstructor
 public class StockPriceMDB {
 
   public static final String EVERY_5_SECONDS = "0/5 * * * * ?";
 
   private final Producer<String, byte[]> producer;
-  private final ConsumerConfig<String, byte[]> consumerConfig;
 
-  public void consume(@Observes StartupEvent ev) {
-    Consumers.consume(this.consumerConfig
+  public StockPriceMDB(Producer<String, byte[]> producer, ConsumerConfig<String, byte[]> consumerConfig) {
+    this.producer = producer;
+    Consumers.consume(consumerConfig
         .toBuilder()
         .topics("stock_changed")
         .consumers(3)
@@ -40,13 +37,22 @@ public class StockPriceMDB {
             .delay(Duration.ofSeconds(29))
             .build()
         )
-        .batchCallback((consumer, records, e) -> {
-          for (final var record : records) {
-//            throw new RuntimeException("an error occurred");
-            log.info("key={}, value={}", record.key(), new String(record.value()));
-          }
-        })
+        .recoverCallback(recover())
+        .batchCallback(consume())
         .build());
+  }
+
+  BatchConsumeCallback<String, byte[]> consume() {
+    return (consumer, records, e) -> {
+      for (final var record : records) {
+//            throw new RuntimeException("an error occurred");
+        log.info("key={}, value={}", record.key(), new String(record.value()));
+      }
+    };
+  }
+
+  RecoverCallback<String, byte[]> recover() {
+    return (record, lastFailure) -> {};
   }
 
   @Scheduled(cron = EVERY_5_SECONDS)
