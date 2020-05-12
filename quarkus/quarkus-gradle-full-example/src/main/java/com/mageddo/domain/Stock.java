9c77624df255bf58e9080105617e9b93cfc0649c
Elvis de Freitas <edigitalb@gmail.com>
2020-05-12 02:18:40 -0300

    jdbi configured

--- quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/domain/Stock.java
+++ quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/domain/Stock.java
@@ -3,15 +3,16 @@ package com.mageddo.domain;
 import java.math.BigDecimal;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
 
-@Value
+//@Value
+@Data
 @Builder
-@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+//@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 @AllArgsConstructor
 @RegisterForReflection
 public class Stock {
