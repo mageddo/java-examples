1ca2ebf79c899013530284d9958cb868b331cfd7
Elvis de Freitas Souza <edigitalb@gmail.com>
2020-05-12 12:31:07 -0300

    replace class on jar example

---
+++ gradle-examples/classpath/src/main/java/com/mageddo/classpath/Main.java
@@ -0,0 +1,9 @@
+package com.mageddo.classpath;
+
+import org.apache.commons.lang3.EnumUtils;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(EnumUtils.newMethod());
+  }
+}
