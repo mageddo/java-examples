6dc2e54d7095827cd7cbaefa6f1ea7707b7c5307
Elvis de Freitas <edigitalb@gmail.com>
2020-05-14 20:54:28 -0300

    creating websocket api with publisher

---
+++ quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/resource/PriceResource.java
@@ -0,0 +1,71 @@
+package com.mageddo.resource;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.mageddo.domain.Stock;
+import com.mageddo.service.StockPriceService;
+
+import org.jboss.resteasy.annotations.SseElementType;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+import io.quarkus.scheduler.Scheduled;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * A simple resource retrieving the "in-memory" "my-data-stream" and sending the items to a server sent event.
+ */
+@Path("/prices")
+@RequiredArgsConstructor
+public class PriceResource {
+
+  private final StockPriceService stockPriceService;
+  private final StockPricePublisher publisher = new StockPricePublisher();
+
+  @GET
+  @Path("/stream")
+  @Produces(MediaType.SERVER_SENT_EVENTS) // denotes that server side events (SSE) will be produced
+  @SseElementType("text/plain") // denotes that the contained data, within this SSE, is just regular text/plain data
+  public Publisher<String> stream() {
+    return this.publisher;
+  }
+
+  @POST
+  @Consumes(MediaType.TEXT_PLAIN)
+  public void addPrice(BigDecimal price) {
+    this.stockPriceService.updateStockPrice(Stock
+        .builder()
+        .symbol("PAGS")
+        .price(price)
+        .build()
+    );
+  }
+
+  @Scheduled(every = "PT3S")
+  public void refresh(){
+    this.publisher.notify(this.stockPriceService.find().get(0).getPrice());
+  }
+
+  private class StockPricePublisher implements Publisher<String> {
+
+    private List<Subscriber<?super String>> subscribers = new ArrayList<>();
+
+    @Override
+    public void subscribe(Subscriber<? super String> s) {
+      this.subscribers.add(s);
+    }
+
+    public void notify(BigDecimal price){
+      this.subscribers.forEach(it -> it.onNext(String.valueOf(price)));
+    }
+  }
+}
