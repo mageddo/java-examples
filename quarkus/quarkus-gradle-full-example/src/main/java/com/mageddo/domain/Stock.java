7f66b42a88dbcc11840f10b97c9dfbeaedc7afe6
Elvis de Freitas <edigitalb@gmail.com>
2020-05-14 21:11:28 -0300

    app it's working

--- quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/domain/Stock.java
+++ quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/domain/Stock.java
@@ -2,15 +2,14 @@ package com.mageddo.domain;
 
 import java.math.BigDecimal;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
 
-@Value
+@Data
 @Builder
-@NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 @AllArgsConstructor
 public class Stock {
   private String symbol;
