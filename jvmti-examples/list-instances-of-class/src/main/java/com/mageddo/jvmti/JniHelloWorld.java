50647d715b86caf9990311476434a1ff8bf745dd
unknown <edigitalb@gmail.com>
2020-06-15 22:39:07 -0300

    fixing run

--- jvmti-examples/list-instances-of-class/src/main/java/com/mageddo/jvmti/JniHelloWorld.java
+++ jvmti-examples/list-instances-of-class/src/main/java/com/mageddo/jvmti/JniHelloWorld.java
@@ -5,7 +5,7 @@ public class JniHelloWorld {
   public static void main(String[] args) {
     FruitInstanceMaker.makeInstances(5);
     System.out.println("Hello World from " + JniHelloWorld.class.getName());
-    System.out.println("instances: " + countInstances(JniHelloWorld.class));
+    System.out.println("instances: " + countInstances(Fruit.class));
   }
 
   private static native int countInstances(Class klass);
