901c1d2e27ab1dc8d3e6de65dfc7c37ae33b5883
Elvis de Freitas <edigitalb@gmail.com>
2020-06-18 02:00:15 -0300

    processing message

--- jvmti-examples/jvm-class-agent/jvmti-native-access/src/main/java/com/mageddo/jvmti/agents/entrypoint/ClassInstancesFinderResource.java
+++ jvmti-examples/jvm-class-agent/jvmti-native-access/src/main/java/com/mageddo/jvmti/agents/entrypoint/ClassInstancesFinderResource.java
@@ -2,13 +2,16 @@ package com.mageddo.jvmti.agents.entrypoint;
 
 import com.mageddo.jvmti.classdelegate.ClassInstanceService;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import net.metzweb.tinyserver.Request;
 import net.metzweb.tinyserver.Response;
 import net.metzweb.tinyserver.TinyServer;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+@Slf4j
 @Singleton
 public class ClassInstancesFinderResource implements Response {
 
@@ -24,8 +27,17 @@ public class ClassInstancesFinderResource implements Response {
   @Override
   @SneakyThrows
   public void callback(Request request) {
-    final String clazzName = request.getData();
-    final int found = this.classInstanceService.scan(Class.forName(clazzName.trim()));
-    request.write(String.format("%d instances found", found));
+    try {
+      final String clazzName = request.getData();
+      final int found = this.classInstanceService.scan(Class.forName(clazzName.trim()));
+      request.write(String.format("%d instances found", found));
+    } catch (Exception e){
+      log.warn("status=can't-find-class, class={}, e", request.getData(), e);
+      request.write(String.format(
+        "couldn't find class=%s, msg=%s",
+        request.getData(),
+        ExceptionUtils.getRootCauseMessage(e)
+      ));
+    }
   }
 }
