7956ba835c697b8733a92236616910f7a3da64a5
unknown <edigitalb@gmail.com>
2020-06-19 00:49:20 -0300

    implementing remote instance class

--- jvmti-examples/jvm-class-agent/jvmti-native-access/src/main/java/com/mageddo/jvmti/RemoteClassInstanceService.java
+++ jvmti-examples/jvm-class-agent/jvmti-native-access/src/main/java/com/mageddo/jvmti/RemoteClassInstanceService.java
@@ -1,15 +1,34 @@
 package com.mageddo.jvmti;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mageddo.jvmti.classdelegate.InstanceId;
 import com.mageddo.jvmti.classdelegate.scanning.InstanceFilter;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.List;
 
 @Slf4j
 @Singleton
 public class RemoteClassInstanceService implements ClassInstanceService {
+
+  private final ObjectMapper objectMapper;
+  private final OkHttpClient client;
+  private final HttpUrl baseUri;
+
+  @Inject
+  public RemoteClassInstanceService(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+    this.client = new OkHttpClient();
+    this.baseUri = HttpUrl.parse("http://localhost:8384");
+  }
 
   @Override
   public InstanceValue getFieldValue(InstanceId id, FieldId fieldId) {
@@ -36,8 +55,21 @@ public class RemoteClassInstanceService implements ClassInstanceService {
     throw new UnsupportedOperationException();
   }
 
+  @SneakyThrows
   @Override
   public List<InstanceValue> scanAndGetValues(ClassId classId) {
-    throw new UnsupportedOperationException();
+    final Response res = this.client
+      .newCall(new Request
+        .Builder()
+        .get()
+        .url(this.baseUri
+          .newBuilder()
+          .addQueryParameter("class", classId.getClassName())
+          .build()
+        )
+        .build()
+      )
+      .execute();
+    return this.objectMapper.readValue(res.body().string(), new TypeReference<List<InstanceValue>>(){});
   }
 }
