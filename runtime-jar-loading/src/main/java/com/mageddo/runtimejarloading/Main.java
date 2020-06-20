0738792cd309927c7370393435981563c9e5c7a7
Elvis de Freitas <edigitalb@gmail.com>
2020-06-20 12:57:09 -0300

    refactoring

--- runtime-jar-loading/src/main/java/com/mageddo/runtimejarloading/Main.java
+++ runtime-jar-loading/src/main/java/com/mageddo/runtimejarloading/Main.java
@@ -1,6 +1,5 @@
 package com.mageddo.runtimejarloading;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,26 +8,29 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-  public static void main(String[] args) throws ClassNotFoundException, MalformedURLException,
-      NoSuchMethodException, IllegalAccessException, InvocationTargetException,
-      InstantiationException {
-    final URLClassLoader classLoader = createClassLoader(Paths.get(args[0]));
-    final Class<?> helloWorldClass = Class.forName(
-        "com.mageddo.runtimejarloading.helloworldlibrary.HelloWorldLibrary",
-        true,
-        classLoader
+  public static void main(String[] args) throws Exception {
+
+    final URLClassLoader jarClassLoader = createClassLoader(Paths.get(args[0]));
+    final Class<?> helloWorldClass = loadClass(
+        jarClassLoader,
+        "com.mageddo.runtimejarloading.helloworldlibrary.HelloWorldLibrary"
     );
     final Object instance = helloWorldClass.getDeclaredConstructor().newInstance();
     final Method method = helloWorldClass.getDeclaredMethod("helloWorld");
-    try {
-      final Object result = method.invoke(instance);
-      System.out.printf("result: %s%n", result);
-    } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-      throw new RuntimeException(e);
-    }
+    final Object result = method.invoke(instance);
+    System.out.printf("result: %s%n", result);
+
   }
 
-  private static URLClassLoader createClassLoader(Path jar) throws MalformedURLException {
+  static Class<?> loadClass(URLClassLoader classLoader, String className) throws ClassNotFoundException {
+    return Class.forName(
+        className,
+        true,
+        classLoader
+    );
+  }
+
+  static URLClassLoader createClassLoader(Path jar) throws MalformedURLException {
     return new URLClassLoader(
         String.format("%s-classloader", jar.getFileName()),
         new URL[]{jar.normalize().toUri().toURL()},
