fc571e3c6a4d5eb06424e0294a9f0bc043665582
Elvis de Freitas <edigitalb@gmail.com>
2020-06-20 12:53:56 -0300

    setup docs

---
+++ runtime-jar-loading/README.md
@@ -0,0 +1,9 @@
+# runtime-jar-loading
+Programmatically loading jar at running JVM
+
+# Running it
+```bash
+$ ./gradlew build &&\
+ java -jar ./build/libs/runtime-jar-loading.jar \
+ ./hello-world-library/build/libs/hello-world-library.jar 
+```
--- runtime-jar-loading/hello-world-library/src/main/java/com/mageddo/runtimejarloading/helloworldlibrary/HelloWorldLibrary.java
+++ runtime-jar-loading/hello-world-library/src/main/java/com/mageddo/runtimejarloading/helloworldlibrary/HelloWorldLibrary.java
@@ -2,6 +2,6 @@ package com.mageddo.runtimejarloading.helloworldlibrary;
 
 public class HelloWorldLibrary {
   public String helloWorld() {
-    return this.getClass().getName() + ": Hello World!!";
+    return this.getClass().getName() + ": Hello World!!!";
   }
 }
