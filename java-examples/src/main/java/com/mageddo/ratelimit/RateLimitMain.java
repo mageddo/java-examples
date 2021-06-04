5e97bde7a259f26efa6a91f7ef44778928c19a60
Elvis Souza <edigitalb@gmail.com>
2021-06-04 17:42:17 -0300

    Ratelimit POC (#19)
    * setup wrapper
    
    * rate limit poc
    
    * rate limit poc

---
+++ java-examples/src/main/java/com/mageddo/ratelimit/RateLimitMain.java
@@ -0,0 +1,55 @@
+package com.mageddo.ratelimit;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+public class RateLimitMain {
+
+  public static void main(String[] args) throws InterruptedException {
+
+    final var limiter = RateLimiter.create(10.0);
+
+    Thread.sleep(100);
+
+    final var map = new HashMap<String, AtomicInteger>();
+    for (int i = 0; true; i++) {
+
+      final var acquired = limiter.tryAcquire(1);
+      final var t = String.format("%s", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+      final var acquiredKey = String.format("%s-acquired", t);
+      final var notAcquiredKey = String.format("%s-not", t);
+
+      if (acquired) {
+        map.compute(acquiredKey, increment());
+
+        final var acquiredInThisSecond = String.valueOf(map.get(acquiredKey));
+        final var notAcquiredInThisSecond = String.valueOf(map.get(notAcquiredKey));
+        if (Objects.equals(acquiredInThisSecond, "10")) {
+          System.out.printf(
+              "acquired=%b, now=%s, acquiredAtThisSecond=%s, notAcquired=%s, %n",
+              acquired, t, acquiredInThisSecond, notAcquiredInThisSecond
+          );
+        }
+      } else {
+        map.compute(notAcquiredKey, increment());
+      }
+    }
+
+  }
+
+  private static BiFunction<String, AtomicInteger, AtomicInteger> increment() {
+    return (k, v) -> {
+      if (v == null) {
+        return new AtomicInteger(1);
+      }
+      v.incrementAndGet();
+      return v;
+    };
+  }
+}
