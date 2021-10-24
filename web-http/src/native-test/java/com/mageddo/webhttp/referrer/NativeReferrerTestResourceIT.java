ad3bb6fd3a01dcb594a1c13df3be14caf0128578
Elvis de Freitas <edigitalb@gmail.com>
2021-10-24 13:52:22 -0300

    initial commit

---
+++ web-http/src/native-test/java/com/mageddo/webhttp/referrer/NativeReferrerTestResourceIT.java
@@ -0,0 +1,9 @@
+package com.mageddo.webhttp.referrer;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReferrerTestResourceIT extends ReferrerTestResourceTest {
+
+    // Execute the same tests but in native mode.
+}
