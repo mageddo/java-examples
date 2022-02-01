380b33af44b9af300cf82ae62a68171f41aea326
Elvis Souza <edigitalb@gmail.com>
2022-02-01 11:59:22 -0300

    setup rate limiter example (#30)

---
+++ failsafe/src/test/java/com/mageddo/failsafe/RateLimitTest.java
@@ -0,0 +1,47 @@
+package com.mageddo.failsafe;
+
+import dev.failsafe.Failsafe;
+import dev.failsafe.RateLimitExceededException;
+import dev.failsafe.RateLimiter;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * https://failsafe.dev/rate-limiter/
+ */
+public class RateLimitTest {
+
+  @Test
+  void mustLimitRequestsAt3PerSecond() {
+    // arrange
+    RateLimiter<Object> limiter = RateLimiter.burstyBuilder(3, Duration.ofSeconds(1)).build();
+
+    // act
+    // assert
+    this.runSuccess(limiter);
+    this.runSuccess(limiter);
+    this.runSuccess(limiter);
+    assertThrows(RateLimitExceededException.class, () -> this.runSuccess(limiter));
+
+    Threads.sleep(1001);
+
+    this.runSuccess(limiter);
+    this.runSuccess(limiter);
+    this.runSuccess(limiter);
+    assertThrows(RateLimitExceededException.class, () -> this.runSuccess(limiter));
+
+  }
+
+  void runSuccess(RateLimiter<Object> limiter) {
+    Failsafe
+        .with(limiter)
+        .get(() -> LocalDateTime.now().toString());
+  }
+
+}
