7901677c8054ee06dc0d260b1a3148974dfdbeca
Elvis de Freitas <edigitalb@gmail.com>
2023-01-27 22:26:10 -0300

    upggrades

--- quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/GraalConfig.java
+++ quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/GraalConfig.java
@@ -1,13 +1,13 @@
-package com.mageddo;
-
-import nativeimage.Reflection;
-
-@Reflection(scanPackage = "com.mageddo.domain",
-    publicMethods = true,
-    declaredMethods = true,
-    declaredFields = true,
-    declaredConstructors = true,
-    publicConstructors = true
-)
-public class GraalConfig {
-}
+//package com.mageddo;
+//
+//import nativeimage.Reflection;
+//
+//@Reflection(scanPackage = "com.mageddo.domain",
+//    publicMethods = true,
+//    declaredMethods = true,
+//    declaredFields = true,
+//    declaredConstructors = true,
+//    publicConstructors = true
+//)
+//public class GraalConfig {
+//}
