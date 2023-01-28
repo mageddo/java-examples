dc0bf4b38d4c19d6507fa9a9414011db2d17ecc2
Elvis de Freitas <edigitalb@gmail.com>
2023-01-27 22:06:54 -0300

    quarkus 2 simple example

---
+++ quarkus/quarkus-2-simple/src/native-test/java/com/mageddo/HelloWorldIT.java
@@ -0,0 +1,8 @@
+package com.mageddo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloWorldIT extends HelloWorldTest {
+    // Execute the same tests but in packaged mode.
+}
