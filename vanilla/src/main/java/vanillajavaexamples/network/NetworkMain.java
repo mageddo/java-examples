27cc06a2b0c3330016a4b789dbe1fc980a8d3751
Elvis de Freitas <edigitalb@gmail.com>
2023-02-23 22:19:45 -0300

    network sampe

---
+++ vanilla/src/main/java/vanillajavaexamples/network/NetworkMain.java
@@ -0,0 +1,56 @@
+package vanillajavaexamples.network;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.stream.Collectors;
+
+/**
+name=veth0f553e2, isUp=true, virtual=false, loopback=false, mac=26:dd:93:43:45:7b, interfaceAddresses=[/fe80:0:0:0:24dd:93ff:fe43:457b%veth0f553e2/64 [null]], inetAddresses=[/fe80:0:0:0:24dd:93ff:fe43:457b%veth0f553e2]
+name=veth3df7c92, isUp=true, virtual=false, loopback=false, mac=26:29:94:cc:a7:1e, interfaceAddresses=[/fe80:0:0:0:2429:94ff:fecc:a71e%veth3df7c92/64 [null]], inetAddresses=[/fe80:0:0:0:2429:94ff:fecc:a71e%veth3df7c92]
+name=vethe77109f, isUp=true, virtual=false, loopback=false, mac=7e:4d:61:63:5c:47, interfaceAddresses=[/fe80:0:0:0:7c4d:61ff:fe63:5c47%vethe77109f/64 [null]], inetAddresses=[/fe80:0:0:0:7c4d:61ff:fe63:5c47%vethe77109f]
+name=docker0, isUp=true, virtual=false, loopback=false, mac=02:42:39:ca:06:52, interfaceAddresses=[/fe80:0:0:0:42:39ff:feca:652%docker0/64 [null], /172.17.0.1/16 [/172.17.255.255]], inetAddresses=[/fe80:0:0:0:42:39ff:feca:652%docker0, /172.17.0.1]
+name=br-b41b674329df, isUp=false, virtual=false, loopback=false, mac=02:42:f0:93:2e:3a, interfaceAddresses=[/192.168.176.1/20 [/192.168.191.255]], inetAddresses=[/192.168.176.1]
+name=docker_gwbridge, isUp=true, virtual=false, loopback=false, mac=02:42:11:d0:c6:1e, interfaceAddresses=[/fe80:0:0:0:42:11ff:fed0:c61e%docker_gwbridge/64 [null], /192.168.48.1/20 [/192.168.63.255]], inetAddresses=[/fe80:0:0:0:42:11ff:fed0:c61e%docker_gwbridge, /192.168.48.1]
+name=br-55ca16d98940, isUp=true, virtual=false, loopback=false, mac=02:42:8a:a8:45:4b, interfaceAddresses=[/fe80:0:0:0:42:8aff:fea8:454b%br-55ca16d98940/64 [null], /172.157.5.1/16 [/172.157.255.255]], inetAddresses=[/fe80:0:0:0:42:8aff:fea8:454b%br-55ca16d98940, /172.157.5.1]
+name=eno1, isUp=true, virtual=false, loopback=false, mac=e0:d5:5e:b8:7a:9a, interfaceAddresses=[/fe80:0:0:0:7ad4:cdd9:f8ac:147e%eno1/64 [null], /2804:14c:22:99a3:0:0:0:1001%eno1/128 [null], /2804:14c:22:99a3:7e97:b9b5:fe24:ac18%eno1/64 [null], /2804:14c:22:99a3:1618:460:ce4d:c9d3%eno1/64 [null], /192.168.0.128/24 [/192.168.0.255]], inetAddresses=[/fe80:0:0:0:7ad4:cdd9:f8ac:147e%eno1, /2804:14c:22:99a3:0:0:0:1001%eno1, /2804:14c:22:99a3:7e97:b9b5:fe24:ac18%eno1, /2804:14c:22:99a3:1618:460:ce4d:c9d3%eno1, /192.168.0.128]
+name=lo, isUp=true, virtual=false, loopback=true, mac=null, interfaceAddresses=[/0:0:0:0:0:0:0:1%lo/128 [null], /127.0.0.1/8 [null]], inetAddresses=[/0:0:0:0:0:0:0:1%lo, /127.0.0.1]
+ */
+public class NetworkMain {
+
+
+  public static void main(String[] args) throws SocketException {
+    NetworkInterface
+      .networkInterfaces()
+      .forEach(it -> {
+
+        try {
+          final var hardwareIp = toString(it.getHardwareAddress());
+          final var interfaceAddresses = it.getInterfaceAddresses();
+          final var inetAddresses = it.inetAddresses().collect(Collectors.toList());
+
+
+          System.out.printf(
+            "name=%s, isUp=%b, virtual=%b, loopback=%b, mac=%s, interfaceAddresses=%s, inetAddresses=%s%n",
+            it.getName(), it.isUp(), it.isVirtual(), it.isLoopback(), hardwareIp, interfaceAddresses, inetAddresses
+          );
+
+        } catch (Exception e){
+          throw new RuntimeException(e);
+        }
+
+      });
+  }
+
+  private static String toString(byte[] addr) {
+    if(addr == null){
+      return null;
+    }
+    final var sb = new StringBuilder();
+    for (byte b : addr) {
+      sb.append(String.format("%02x", Byte.toUnsignedInt(b)));
+      sb.append(':');
+    }
+    sb.deleteCharAt(sb.length()-1);
+    return sb.toString();
+  }
+}
