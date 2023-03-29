39ec42d246420146d3bf9a39164655337cd79ee1
Elvis de Freitas <edigitalb@gmail.com>
2023-03-29 18:24:48 -0300

    simplify

--- spring-examples/src/main/java/com/mageddo/springexamples/ex01/Props.java
+++ spring-examples/src/main/java/com/mageddo/springexamples/ex01/Props.java
@@ -4,9 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "root")
 public class Props {
 
