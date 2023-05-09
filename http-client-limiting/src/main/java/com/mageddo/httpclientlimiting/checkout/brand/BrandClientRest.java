722a6264150a98a39a1c9b5cc28da7f6d51a610a
Elvis de Freitas <edigitalb@gmail.com>
2023-05-08 21:14:43 -0300

    adjusts

--- http-client-limiting/src/main/java/com/mageddo/httpclientlimiting/checkout/brand/BrandClientRest.java
+++ http-client-limiting/src/main/java/com/mageddo/httpclientlimiting/checkout/brand/BrandClientRest.java
@@ -1,5 +1,7 @@
 package com.mageddo.httpclientlimiting.checkout.brand;
 
+import java.time.Duration;
+
 import com.mageddo.httpclientlimiting.brand.resource.reqres.PaymentAuthorizationReq;
 import com.mageddo.httpclientlimiting.brand.resource.reqres.PaymentAuthorizationRes;
 import com.mageddo.httpclientlimiting.checkout.Payment;
@@ -8,10 +10,15 @@ import org.apache.commons.lang3.Validate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
+import dev.failsafe.Failsafe;
+import dev.failsafe.RetryPolicy;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BrandClientRest implements BrandClient {
@@ -20,6 +27,31 @@ public class BrandClientRest implements BrandClient {
 
   @Override
   public boolean authorize(Payment paymentReq) {
+    return Failsafe
+        .with(
+            RetryPolicy.builder()
+                .withMaxAttempts(3)
+                .withDelay(Duration.ofMillis(40))
+                .handle(ResourceAccessException.class)
+                .onRetry(it -> {
+                  log.debug(
+                      "status=retry, attempt={}, paymentId={}",
+                      it.getAttemptCount(), paymentReq.getId()
+                  );
+                })
+                .build()
+        )
+//        .onComplete(it -> System.out.println("completed :" + it))
+        .onFailure(it -> {
+          log.debug("status=failed, paymentId={}", paymentReq.getId(), it.getException());
+        })
+        .onSuccess(it -> {
+          log.debug("status=success, paymentId={}", paymentReq.getId());
+        })
+        .get((ctx) -> this.doAuthorize(paymentReq));
+  }
+
+  private Boolean doAuthorize(Payment paymentReq) {
     final var req = PaymentAuthorizationReq
         .builder()
         .id(paymentReq.getId())
