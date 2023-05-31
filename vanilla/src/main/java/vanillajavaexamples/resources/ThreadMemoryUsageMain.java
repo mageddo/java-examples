dd63a8d295f3edbdffde6bf1daa3f3129f08cdeb
Elvis Souza <edigitalb@gmail.com>
2023-05-31 01:49:55 -0300

    Resources (#61)
    * new classe sample
    
    * updating sample
    
    * new  tests

---
+++ vanilla/src/main/java/vanillajavaexamples/resources/ThreadMemoryUsageMain.java
@@ -0,0 +1,64 @@
+package vanillajavaexamples.resources;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ *
+ * $ ./gradlew build shadowJar -x check && docker-compose up thread-memory-usage --build
+ */
+public class ThreadMemoryUsageMain {
+
+  static final Thread[] threads = new Thread[30_000];
+  static final List<byte[]> data = new LinkedList<>();
+
+  public static void main(String[] args) throws Exception {
+
+    System.out.printf("started with pid=%d%n", ProcessHandle.current().pid());
+    System.out.println("> Before threads allocating");
+    System.out.println(MemoryUtils.dumpMemory());
+
+    System.out.println("> Allocating threads");
+    for (int i = 0; i < threads.length; i++) {
+
+      final var thread = threads[i] = new Thread(() -> {
+        while (true) {
+          randomSleep();
+        }
+      });
+      thread.setDaemon(true);
+      thread.start();
+
+      if(i % 1000 == 0){
+        System.out.println(MemoryUtils.dumpMemory());
+      }
+
+    }
+    System.out.println("> After threads allocated");
+
+    while (true){
+      Thread.sleep(5_000);
+      System.out.println(MemoryUtils.dumpMemory());
+      allocateRam();
+    }
+  }
+
+  private static void allocateRam() {
+    final var mb = 50;
+    System.out.printf("allocating %dmb%n", mb);
+    final var b = new byte[mb * 1024 * 1024];
+    for (int i = 0; i < b.length; i++) {
+      b[i] = (byte) System.nanoTime();
+    }
+    data.add(b);
+  }
+
+  private static void randomSleep() {
+    try {
+      Thread.sleep((long) (Math.random() * 1000));
+    } catch (InterruptedException e) {
+      throw new RuntimeException();
+    }
+  }
+}
