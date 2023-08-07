7512c636c75802e4bfeabddc196bf2612f41c16f
Elvis de Freitas <edigitalb@gmail.com>
2023-08-07 18:24:31 -0300

    new configs

--- observability/grafana-prometheus-open-telemetry/coffee-maker-checkout/src/main/java/com/mageddo/coffeemaker/checkout/CoffeeCheckoutReq.java
+++ observability/grafana-prometheus-open-telemetry/coffee-maker-checkout/src/main/java/com/mageddo/coffeemaker/checkout/CoffeeCheckoutReq.java
@@ -3,9 +3,12 @@ package com.mageddo.coffeemaker.checkout;
 import java.math.BigDecimal;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class CoffeeCheckoutReq {
   private String coffeeName;
   private BigDecimal amount;
+
 }
