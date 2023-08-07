afd2ad0c247758422e0ae3a3b399dccbe40e70dc
Elvis de Freitas <edigitalb@gmail.com>
2023-08-07 15:03:48 -0300

    setup grafana stack

---
+++ observability/grafana-prometheus-open-telemetry/coffee-maker-checkout/README.md
@@ -0,0 +1,10 @@
+## Build
+
+```bash
+$ ./gradlew build shadowJar && docker-compose build
+```
+
+## Reference 
+* https://opentelemetry.io/docs/instrumentation/java/automatic/
+* https://opentelemetry.io/docs/instrumentation/java/automatic/annotations/
+* https://github.com/open-telemetry/opentelemetry-java/blob/main/sdk-extensions/autoconfigure/README.md#prometheus-exporter
--- observability/grafana-prometheus-open-telemetry/coffee-maker-checkout/src/main/java/com/mageddo/coffeemaker/checkout/CoffeeCheckoutController.java
+++ observability/grafana-prometheus-open-telemetry/coffee-maker-checkout/src/main/java/com/mageddo/coffeemaker/checkout/CoffeeCheckoutController.java
@@ -2,11 +2,13 @@ package com.mageddo.coffeemaker.checkout;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class CoffeeCheckoutController {
 
   private final CoffeeCheckoutService coffeeCheckoutService;
@@ -17,7 +19,7 @@ public class CoffeeCheckoutController {
   }
 
   @WithSpan
-  @PostMapping
+  @PostMapping("/checkout")
   public void checkout(CoffeeCheckoutReq c){
     this.coffeeCheckoutService.checkout(c);
   }
