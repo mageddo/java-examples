168e0e81de93b7f0f1b8af60692be20eae200d77
Elvis de Freitas <edigitalb@gmail.com>
2023-08-07 14:10:20 -0300

    adjusts

--- observability/grafana-prometheus-open-telemetry/coffee-maker-checkout/src/main/java/com/mageddo/coffeemaker/checkout/Main.java
+++ observability/grafana-prometheus-open-telemetry/coffee-maker-checkout/src/main/java/com/mageddo/coffeemaker/checkout/Main.java
@@ -1,9 +1,9 @@
 package com.mageddo.coffeemaker.checkout;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootConfiguration
+@SpringBootApplication
 public class Main {
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
