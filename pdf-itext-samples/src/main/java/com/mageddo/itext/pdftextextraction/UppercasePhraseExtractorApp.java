a0e3d19a3f7c9976c1b95828dc0f979f7bacac74
Elvis de Freitas <edigitalb@gmail.com>
2024-04-01 13:44:37 -0300

    all done

--- pdf-itext-samples/src/main/java/com/mageddo/itext/pdftextextraction/UppercasePhraseExtractorApp.java
+++ pdf-itext-samples/src/main/java/com/mageddo/itext/pdftextextraction/UppercasePhraseExtractorApp.java
@@ -2,6 +2,7 @@ package com.mageddo.itext.pdftextextraction;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import com.itextpdf.kernel.pdf.PdfDocument;
@@ -24,7 +25,7 @@ public class UppercasePhraseExtractorApp {
     final var document = new PdfDocument(new PdfReader(fileName));
     try {
       final var sentences = extractUppercaseWordSequencesFromPdf(document);
-      System.out.println(sentences);
+      System.out.println(new LinkedHashSet<>(sentences));
     } finally {
       document.close();
     }
@@ -39,12 +40,12 @@ public class UppercasePhraseExtractorApp {
       // executadas no retorno do texto
       final var strategy = new SimpleTextExtractionStrategy();
       final var text = PdfTextExtractor.getTextFromPage(document.getPage(i), strategy);
-//      sentences.addAll(extractUppercaseWordsSentenceFromText(text));
-      final var s = extractUppercaseWordsSentenceFromText(text);
-      System.out.println("--------------------------------------");
-      System.out.println(s);
-      System.out.println("page: " + i);
-      System.out.println("=====================================");
+      sentences.addAll(extractUppercaseWordsSentenceFromText(text));
+//      final var s = extractUppercaseWordsSentenceFromText(text);
+//      System.out.println("--------------------------------------");
+//      System.out.println(s);
+//      System.out.println("page: " + i);
+//      System.out.println("=====================================");
 
     }
     return sentences;
