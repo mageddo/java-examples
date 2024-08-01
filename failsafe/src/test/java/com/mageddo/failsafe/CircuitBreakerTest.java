05ea0081db575cf8f0bd65167b57a1e7cb0cfc96
Elvis de Freitas <edigitalb@gmail.com>
2024-08-01 17:55:27 -0300

    some tests scenarios

--- failsafe/src/test/java/com/mageddo/failsafe/CircuitBreakerTest.java
+++ failsafe/src/test/java/com/mageddo/failsafe/CircuitBreakerTest.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 
 import dev.failsafe.CircuitBreaker;
+import dev.failsafe.CircuitBreaker.State;
 import dev.failsafe.CircuitBreakerOpenException;
 import dev.failsafe.Failsafe;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,6 +19,94 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * https://failsafe.dev/circuit-breaker/
  */
 public class CircuitBreakerTest {
+
+  @Test
+  void mustThrowOpenCircuitExceptionWhenTryingToUseOpenCircuit(){
+
+    final var stats = new Stats();
+    final var circuit = CircuitBreaker.<String>builder()
+        .handle(UncheckedIOException.class)
+        .withFailureThreshold(2)
+        .withSuccessThreshold(2)
+        .withDelay(Duration.ofMillis(500))
+        .build();
+
+    assertEquals(State.CLOSED, circuit.getState());
+
+    calcStats(stats, () -> runError(circuit));
+    assertEquals(State.CLOSED, circuit.getState());
+
+    calcStats(stats, () -> runError(circuit));
+    assertEquals(State.OPEN, circuit.getState());
+
+    calcStats(stats, () -> runSuccess(circuit));
+    assertEquals(State.OPEN, circuit.getState());
+
+    assertEquals(2, stats.error);
+    assertEquals(1, stats.openCircuit);
+    assertEquals(0, stats.success);
+  }
+
+  @Test
+  void mustOpenAfterConfiguredFailures(){
+
+    final var stats = new Stats();
+    final var circuit = CircuitBreaker.<String>builder()
+        .handle(UncheckedIOException.class)
+        .withFailureThreshold(2)
+        .withSuccessThreshold(2)
+        .withDelay(Duration.ofMillis(500))
+        .build();
+
+    assertEquals(State.CLOSED, circuit.getState());
+
+    calcStats(stats, () -> runError(circuit));
+    assertEquals(State.CLOSED, circuit.getState());
+
+    calcStats(stats, () -> runError(circuit));
+    assertEquals(State.OPEN, circuit.getState());
+
+  }
+
+  @Test
+  void mustHalfOpenAfterRunATaskWithSuccessAfterTheConfiguredTime(){
+
+    final var stats = new Stats();
+    final var circuit = CircuitBreaker.<String>builder()
+        .handle(UncheckedIOException.class)
+        .withFailureThreshold(2)
+        .withSuccessThreshold(2)
+        .withDelay(Duration.ofMillis(300))
+        .build();
+
+    circuit.open();
+    assertEquals(State.OPEN, circuit.getState());
+
+    Threads.sleep(500);
+
+    calcStats(stats, () -> runSuccess(circuit));
+    assertEquals(State.HALF_OPEN, circuit.getState());
+
+  }
+
+  @Test
+  void waitDelayTimeAndDontTryToExecuteTaskWontChangeTheState(){
+
+    final var circuit = CircuitBreaker.<String>builder()
+        .handle(UncheckedIOException.class)
+        .withFailureThreshold(2)
+        .withSuccessThreshold(2)
+        .withDelay(Duration.ofMillis(300))
+        .build();
+
+    circuit.open();
+    assertEquals(State.OPEN, circuit.getState());
+
+    Threads.sleep(500);
+
+    assertEquals(State.OPEN, circuit.getState());
+
+  }
 
   @Test
   void mustOpenThenHalfOpenTheCircuit() {
@@ -55,7 +144,7 @@ public class CircuitBreakerTest {
     assertEquals(2, stats.openCircuit);
   }
 
-  void calcStats(Stats stats, Runnable r) {
+  Stats calcStats(Stats stats, Runnable r) {
     try {
       r.run();
       stats.success++;
@@ -64,6 +153,7 @@ public class CircuitBreakerTest {
     } catch (UncheckedIOException e) {
       stats.error++;
     }
+    return stats;
   }
 
   String runError(CircuitBreaker<String> breaker) {
