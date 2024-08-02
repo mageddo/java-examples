2775cd2b4af8efb70f251cebc26ce8f43896bf85
Elvis de Freitas <edigitalb@gmail.com>
2024-08-02 13:14:53 -0300

    new test

--- failsafe/src/test/java/com/mageddo/failsafe/CircuitBreakerTest.java
+++ failsafe/src/test/java/com/mageddo/failsafe/CircuitBreakerTest.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 
 import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
 
 import dev.failsafe.CircuitBreaker;
 import dev.failsafe.CircuitBreaker.State;
@@ -14,6 +15,7 @@ import dev.failsafe.CircuitBreakerOpenException;
 import dev.failsafe.Failsafe;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -167,12 +169,11 @@ public class CircuitBreakerTest {
       Threads.sleep(81);
     }
 
-
   }
 
   /**
    * withSuccessThreshold sobrepoe o withFailureThreshold
-   *
+   * <p>
    * O circuit fica aberto pelo wityDelay,
    * depois fica meio aberto pela quantidade  successThresholdingCapacity - successThreshold,
    * depois fica aberto denovo pelo withDelay
@@ -202,19 +203,121 @@ public class CircuitBreakerTest {
 
   }
 
+  /**
+   * This scenario doesn't makes sense for me, failureThresholdingPeriod is not considered
+   */
+  @Test
+  void mustReopenCircuitAfterThePERIODBetMet() {
+
+    final var ignoredInThisUseCase = 100;
+    final var failureThresholdingPeriod = Duration.ofMillis(100);
+    final var circuit = CircuitBreaker.<String>builder()
+        .handle(UncheckedIOException.class)
+        .withFailureRateThreshold(ignoredInThisUseCase, 100, failureThresholdingPeriod)
+        .withDelay(Duration.ofMillis(80))
+        .build();
+
+    circuit.halfOpen();
+
+
+    for (int i = 0; i < 3; i++) {
+
+      testCircuitOnError(Result.ERROR, State.HALF_OPEN, circuit, 10);
+      Threads.sleep(10);
+      testCircuitOnError(Result.ERROR, State.HALF_OPEN, circuit, 10);
+      Threads.sleep(100);
+
+      testCircuitOnError(Result.ERROR, State.HALF_OPEN, circuit, 79);
+
+      testCircuitOnError(Result.ERROR, State.OPEN, circuit, 1);
+      testCircuitOnError(Result.CIRCUIT_OPEN, State.OPEN, circuit, 35);
+
+      Threads.sleep(81);
+    }
+  }
+
+  @Test
+  void whenFailureExecutionThresholdIsMetMustCloseCircuit() {
+
+    final var ignoredInThisUseCase = 10;
+    final var failureThresholdingPeriod = Duration.ofMillis(100);
+    final var failureExecutionThreshold = 100;
+    final var circuit = CircuitBreaker.<String>builder()
+        .handle(UncheckedIOException.class)
+        .withFailureRateThreshold(ignoredInThisUseCase, failureExecutionThreshold,
+            failureThresholdingPeriod)
+        .withDelay(Duration.ofMillis(80))
+        .build();
+
+    for (int i = 0; i < 2; i++) {
+
+      circuit.halfOpen();
+      testCircuitOnSuccess(Result.SUCCESS, State.HALF_OPEN, circuit, 99);
+      testCircuitOnSuccess(Result.SUCCESS, State.CLOSED, circuit, 1);
+
+    }
+  }
+
+  @Test
+  void whenFailureThresholdingPeriodIsMetMustCloseCircuitButItDontWork() {
+
+    final var ignoredInThisUseCase = 10;
+    final var failureThresholdingPeriod = Duration.ofMillis(100);
+    final var failureExecutionThreshold = 100;
+    final var circuit = CircuitBreaker.<String>builder()
+        .handle(UncheckedIOException.class)
+        .withFailureRateThreshold(ignoredInThisUseCase, failureExecutionThreshold,
+            failureThresholdingPeriod)
+        .withDelay(Duration.ofMillis(80))
+        .build();
+
+
+    circuit.halfOpen();
+
+    for (int i = 0; i < 1; i++) {
+
+      testCircuitOnSuccess(Result.SUCCESS, State.HALF_OPEN, circuit, 90);
+
+      Threads.sleep(120);
+
+      assertThrows(
+          AssertionFailedError.class,
+          () -> {
+            testCircuitOnSuccess(Result.SUCCESS, State.CLOSED, circuit, 1);
+          }
+      );
+
+    }
+  }
+
   void testCircuitOnError(
       final Result expectedResult, final State expectedState,
       final CircuitBreaker<String> circuit, final int times
   ) {
+    testCircuitOn(expectedResult, expectedState, circuit, times, () -> runError(circuit));
+  }
+
+  void testCircuitOnSuccess(
+      final Result expectedResult, final State expectedState,
+      final CircuitBreaker<String> circuit, final int times
+  ) {
+    testCircuitOn(expectedResult, expectedState, circuit, times, () -> runSuccess(circuit));
+  }
+
+  void testCircuitOn(
+      final Result expectedResult, final State expectedState,
+      final CircuitBreaker<String> circuit, final int times, final Runnable runnable
+  ) {
     final var stats = new Stats();
     final var stopWatch = StopWatch.createStarted();
     for (int i = 0; i < times; i++) {
-      assertEquals(expectedResult, calcStats(stats, () -> runError(circuit)));
+      assertEquals(expectedResult, calcStats(stats, runnable));
       assertCircuitState(i, stopWatch, expectedState, circuit.getState());
     }
   }
 
-  private static void assertCircuitState(int i, StopWatch stopWatch, final State expectedState, final State actualState) {
+  private static void assertCircuitState(int i, StopWatch stopWatch, final State expectedState,
+      final State actualState) {
     assertEquals(
         expectedState,
         actualState,
