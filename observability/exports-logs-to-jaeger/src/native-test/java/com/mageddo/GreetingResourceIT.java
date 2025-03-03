0e1d775408e8c00507f0dc6cc1965e7933a541cc
Elvis de Freitas <edigitalb@gmail.com>
2025-03-03 11:13:27 -0300

    setup example

---
+++ observability/exports-logs-to-jaeger/src/native-test/java/com/mageddo/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.mageddo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
