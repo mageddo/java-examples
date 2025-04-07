08efd2d7dd9c59523acfd589af23a293b99f484e
Elvis de Freitas <edigitalb@gmail.com>
2025-04-07 15:36:21 -0300

    fixing test

--- lombok-examples/src/test/java/com/mageddo/lombok/CleanupTest.java
+++ lombok-examples/src/test/java/com/mageddo/lombok/CleanupTest.java
@@ -2,46 +2,66 @@ package com.mageddo.lombok;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 import lombok.Cleanup;
 import lombok.SneakyThrows;
 import lombok.val;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertTrue;
 
 public class CleanupTest {
 
   @Test
-  public void mustReadInputStreamAndClose() throws Exception {
+  public void mustReadInputStreamAndClose() {
 
     // arrange
     val expectedString = "hello world!!!";
-    val in = spy(new ByteArrayInputStream(expectedString.getBytes()));
+    val in = new InputStreamStub(new ByteArrayInputStream(expectedString.getBytes()));
 
     // act
     val readString = readToStringAndClose(in);
 
     // assert
     assertEquals(expectedString, readString);
-    verify(in).close();
+    assertTrue(in.isClosed());
 
   }
 
   @SneakyThrows
   String readToStringAndClose(InputStream in) {
-    @Cleanup val in0 = in;
-    val bout = new ByteArrayOutputStream();
-    val buff = new byte[512];
-    for (; ; ) {
-      val i = in0.read(buff);
-      if (i == -1) {
-        return bout.toString();
-      }
-      bout.write(buff, 0, i);
+    @Cleanup var out = new ByteArrayOutputStream();
+    @Cleanup var in0 = in;
+    IOUtils.copy(in0, out);
+    return out.toString();
+  }
+
+  static class InputStreamStub extends InputStream {
+
+    private boolean closed;
+    private final ByteArrayInputStream delegate;
+
+    public InputStreamStub(ByteArrayInputStream delegate) {
+      this.delegate = delegate;
+    }
+
+    @Override
+    public void close() throws IOException {
+      this.closed = true;
+      super.close();
+    }
+
+    @Override
+    public int read() throws IOException {
+      return this.delegate.read();
+    }
+
+    public boolean isClosed() {
+      return closed;
     }
   }
 }
