5d48936452859e8f255863a227547a28de864f92
Elvis De Freitas Souza <edigitalb@gmail.com>
2025-10-27 19:09:18 -0300

    adjusts

--- logging-spring/src/main/java/com/pagbank/logging/App.java
+++ logging-spring/src/main/java/com/pagbank/logging/App.java
@@ -1,6 +1,11 @@
+package com.pagbank.logging;
+
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @Slf4j
+@SpringBootApplication
 public class App {
   public static void main(String[] args) {
     log.info("action=helloWorld");
