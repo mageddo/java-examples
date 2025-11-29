fa0924e627ac1c46455fd620ac414a030ac33a8f
Elvis Souza <edigitalb@gmail.com>
2025-11-29 17:00:19 -0300

    Javax naming - JDNI Samples (#107)

---
+++ jdni-javax-naming/src/main/java/com/mageddo/jndi/InMemoryContext.java
@@ -0,0 +1,171 @@
+package com.mageddo.jndi;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.naming.Binding;
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NameAlreadyBoundException;
+import javax.naming.NameClassPair;
+import javax.naming.NameNotFoundException;
+import javax.naming.NameParser;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.spi.InitialContextFactory;
+
+public class InMemoryContext implements Context {
+
+  private static final Map<String, Object> REGISTRY = new ConcurrentHashMap<>();
+
+  @Override
+  public Object lookup(String name) throws NamingException {
+    var obj = REGISTRY.get(name);
+    if (obj == null) {
+      throw new NameNotFoundException("Name not found: " + name);
+    }
+    return obj;
+  }
+
+  @Override
+  public void bind(String name, Object obj) throws NamingException {
+    if (REGISTRY.putIfAbsent(name, obj) != null) {
+      throw new NameAlreadyBoundException("Already bound: " + name);
+    }
+  }
+
+  @Override
+  public void rebind(String name, Object obj) {
+    REGISTRY.put(name, obj);
+  }
+
+  @Override
+  public void unbind(String name) {
+    REGISTRY.remove(name);
+  }
+
+  @Override
+  public void close() {
+  }
+
+  @Override
+  public Object lookup(Name name) throws NamingException {
+    return lookup(name.toString());
+  }
+
+  @Override
+  public void bind(Name name, Object obj) throws NamingException {
+    bind(name.toString(), obj);
+  }
+
+  @Override
+  public void rebind(Name name, Object obj) throws NamingException {
+    rebind(name.toString(), obj);
+  }
+
+  @Override
+  public void unbind(Name name) throws NamingException {
+    unbind(name.toString());
+  }
+
+  @Override
+  public void rename(String oldName, String newName) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void rename(Name oldName, Name newName) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public NamingEnumeration<NameClassPair> list(String name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public NamingEnumeration<NameClassPair> list(Name name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public NamingEnumeration<Binding> listBindings(String name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public NamingEnumeration<Binding> listBindings(Name name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void destroySubcontext(String name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void destroySubcontext(Name name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Context createSubcontext(String name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Context createSubcontext(Name name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Object lookupLink(String name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Object lookupLink(Name name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public NameParser getNameParser(String name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public NameParser getNameParser(Name name) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Name composeName(Name name, Name prefix) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String composeName(String name, String prefix) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Object addToEnvironment(String propName, Object propVal) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Object removeFromEnvironment(String propName) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Hashtable<?, ?> getEnvironment() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public String getNameInNamespace() {
+    throw new UnsupportedOperationException();
+  }
+}
---
+++ jdni-javax-naming/src/test/java/com/mageddo/jndi/JavaxNamingTest.java
@@ -0,0 +1,63 @@
+package com.mageddo.jndi;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class JavaxNamingTest {
+
+  static final String JNDI_NAME = "java:global/my/MyService";
+
+  @BeforeAll
+  static void beforeAll(){
+    System.setProperty(
+        Context.INITIAL_CONTEXT_FACTORY,
+        InMemoryContextFactory.class.getName()
+    );
+  }
+
+  @Test
+  void mustRegisterAndLookup() throws Exception {
+    register(new MyService());
+
+    final var service = lookup();
+
+    assertNotNull(service);
+  }
+
+
+  static void register(final MyService service) throws NamingException {
+    final var ctx = new InitialContext();
+    try {
+      ctx.rebind(JNDI_NAME, service);
+    } finally {
+      ctx.close();
+    }
+  }
+
+  public static MyService lookup() throws NamingException {
+    return InitialContext.doLookup(JNDI_NAME);
+  }
+
+  static class MyService {
+
+    private final String name;
+
+    public MyService() {
+      this.name = null;
+    }
+
+    MyService(String name) {
+      this.name = name;
+    }
+
+    void sayHello() {
+      System.out.println("Hello");
+    }
+  }
+}
