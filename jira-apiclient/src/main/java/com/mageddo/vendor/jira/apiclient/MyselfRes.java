466f94e0dbf4106a0b4741ad13f94e2f7c471253
Elvis De Freitas Souza <edigitalb@gmail.com>
2026-05-19 13:42:26 -0300

    adjusts

--- jira-apiclient/src/main/java/com/mageddo/vendor/jira/apiclient/MyselfRes.java
+++ jira-apiclient/src/main/java/com/mageddo/vendor/jira/apiclient/MyselfRes.java
@@ -1,65 +1,71 @@
 package com.mageddo.vendor.jira.apiclient;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record MyselfRes(
 
-  String accountId,
+    String accountId,
 
-  String accountType,
+    String accountType,
 
-  Boolean active,
+    Boolean active,
 
-  ApplicationRoles applicationRoles,
+    ApplicationRoles applicationRoles,
 
-  AvatarUrls avatarUrls,
+    AvatarUrls avatarUrls,
 
-  String displayName,
+    String displayName,
 
-  String emailAddress,
+    String emailAddress,
 
-  Groups groups,
+    Groups groups,
 
-  String key,
+    String key,
 
-  String name,
+    String name,
 
-  String self,
+    String self,
 
-  String timeZone
+    String timeZone
 
 ) {
 
   @Builder
+  @JsonIgnoreProperties(ignoreUnknown = true)
   public record ApplicationRoles(
-    List<Object> items,
-    Integer size
+      List<Object> items,
+      Integer size
   ) {}
 
   @Builder
+  @JsonIgnoreProperties(ignoreUnknown = true)
   public record AvatarUrls(
 
-    @JsonProperty("16x16")
-    String size16x16,
+      @JsonProperty("16x16")
+      String size16x16,
 
-    @JsonProperty("24x24")
-    String size24x24,
+      @JsonProperty("24x24")
+      String size24x24,
 
-    @JsonProperty("32x32")
-    String size32x32,
+      @JsonProperty("32x32")
+      String size32x32,
 
-    @JsonProperty("48x48")
-    String size48x48
+      @JsonProperty("48x48")
+      String size48x48
 
   ) {}
 
   @Builder
   public record Groups(
-    List<Object> items,
-    Integer size
+      List<Object> items,
+      Integer size
   ) {}
 }
