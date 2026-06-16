2d9f6a26c38657cb2d70a33f20cb01ebe5e3c40a
Elvis De Freitas Souza <edigitalb@gmail.com>
2026-06-16 19:31:16 -0300

    adjusts

--- observability/quarkus-observability/src/main/java/com/mageddo/observability/micrometer/TimerHistogramConfigurer.java
+++ observability/quarkus-observability/src/main/java/com/mageddo/observability/micrometer/TimerHistogramConfigurer.java
@@ -1,14 +1,12 @@
 package com.mageddo.observability.micrometer;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
+import jakarta.inject.Singleton;
 
-@Configuration
-public class TimerHistogramConfigurer {
+@Singleton
+public class TimerHistogramConfigurer implements MeterFilter {
 
   private final DistributionStatisticConfig defaults;
 
@@ -16,27 +14,18 @@ public class TimerHistogramConfigurer {
     this.defaults = DistributionStatisticFactory.build();
   }
 
-  @Bean
-  MeterFilter defaultTimerBuckets() {
-    final var defaults = this.defaults;
-    return new MeterFilter() {
-      @Override
-      public DistributionStatisticConfig configure(
-          Meter.Id id, DistributionStatisticConfig config
-      ) {
+  @Override
+  public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+    if (id.getType() != Meter.Type.TIMER) {
+      return config;
+    }
 
-        if (id.getType() != Meter.Type.TIMER) {
-          return config;
-        }
+    final var buckets = config.getServiceLevelObjectiveBoundaries();
+    if (buckets != null && buckets.length > 0) {
+      return config;
+    }
 
-        final var buckets = config.getServiceLevelObjectiveBoundaries();
-        if(buckets != null && buckets.length > 0){
-          return config;
-        }
-
-        return config.merge(defaults);
-      }
-    };
+    return config.merge(defaults);
   }
 
 }
