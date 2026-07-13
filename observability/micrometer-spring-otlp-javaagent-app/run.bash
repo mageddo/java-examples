487be0332d353d1ffc09ade60e2fcdec121f332a
Elvis De Freitas Souza <edigitalb@gmail.com>
2026-07-13 14:55:27 -0300

    adjusts

---
+++ observability/micrometer-spring-otlp-javaagent-app/run.bash
@@ -0,0 +1,9 @@
+#!bin/bash
+
+# para fins de instrumentação e mandar metricas da jvm e libs dela automaticamente
+export OTEL_SERVICE_NAME="micrometer-spring-otlp-javaagent-app"
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://otel-collector.docker:4317
+export OTEL_RESOURCE_ATTRIBUTES="service.namespace=com.mageddo,service.id=1231,service.version=1.0.0-alpha"
+export OTEL_TRACES_SAMPLER="always_on"
+
+java -javaagent:./tmp/otel-agent.jar -jar build/libs/micrometer-spring-otlp-javaagent-app.jar
