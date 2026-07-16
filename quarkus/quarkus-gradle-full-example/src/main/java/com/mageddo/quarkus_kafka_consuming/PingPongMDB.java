1dd01205317bdd6739333c1cc3f15b56aa487ab2
Elvis de Freitas <edigitalb@gmail.com>
2026-07-15 21:06:55 -0300

    adjusts

---
+++ quarkus/quarkus-gradle-full-example/src/main/java/com/mageddo/quarkus_kafka_consuming/PingPongMDB.java
@@ -0,0 +1,40 @@
+package com.mageddo.quarkus_kafka_consuming;
+
+import io.quarkus.scheduler.Scheduled;
+import jakarta.inject.Singleton;
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import java.time.temporal.ChronoUnit;
+
+@Slf4j
+@Singleton
+@RequiredArgsConstructor
+public class PingPongMDB {
+
+  private final Producer<String, byte[]> producer;
+
+  @Retry(
+      delay = 5,
+      delayUnit = ChronoUnit.SECONDS,
+      maxRetries = Integer.MAX_VALUE,
+      retryOn = Exception.class
+  )
+  @Incoming("sys.ping-pong.default")
+  public void consume(ConsumerRecords<String, byte[]> records) {
+    log.info("status=pong, records={}", records.count());
+  }
+
+  @Scheduled(every = "PT3S")
+  public void ping() {
+    this.producer.send(new ProducerRecord<>("sys.ping-pong.default", null));
+  }
+}
