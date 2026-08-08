34e41e58267c70e311adb42dc61363c5f4f50995
Elvis de Freitas <edigitalb@gmail.com>
2026-08-07 23:07:25 -0300

    adjusts

--- orm-ebean/src/main/java/com/mageddo/fruit/dataprovider/FruitRow.java
+++ orm-ebean/src/main/java/com/mageddo/fruit/dataprovider/FruitRow.java
@@ -5,27 +5,32 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
+
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "fruit")
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class FruitRow {
 
   @Id
   @Column(name = "idt_fruit", nullable = false)
-  private UUID id;
+  UUID id;
 
   @Column(name = "nam_name")
-  private String name;
+  String name;
 
   @Column(name = "txt_color")
-  private String color;
+  String color;
 
   @Column(name = "txt_season")
-  private String season;
+  String season;
 }
