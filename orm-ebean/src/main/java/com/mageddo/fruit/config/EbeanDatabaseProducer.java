59565df40088d06b736c1457c63a504fe92a64b2
Elvis de Freitas <edigitalb@gmail.com>
2026-08-07 22:50:28 -0300

    adjusts

--- orm-ebean/src/main/java/com/mageddo/fruit/config/EbeanDatabaseProducer.java
+++ orm-ebean/src/main/java/com/mageddo/fruit/config/EbeanDatabaseProducer.java
@@ -1,25 +1,29 @@
 package com.mageddo.fruit.config;
 
 import io.ebean.Database;
+import io.ebean.DatabaseFactory;
+import io.ebean.config.DatabaseConfig;
 import com.mageddo.fruit.dataprovider.FruitRow;
 import jakarta.inject.Singleton;
+import jakarta.inject.Inject;
 import jakarta.enterprise.inject.Produces;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
 
 @Singleton
+@RequiredArgsConstructor(onConstructor_ = @Inject)
 public class EbeanDatabaseProducer {
 
-  private final Database database;
-
-  public EbeanDatabaseProducer() {
-    this.database = Database.builder()
-      .name("db")
-      .addClass(FruitRow.class)
-      .loadFromProperties()
-      .build();
-  }
+  private final DataSource dataSource;
 
   @Produces
+  @Singleton
   public Database database() {
-    return this.database;
+    final var databaseConfig = new DatabaseConfig();
+    databaseConfig.setName("db");
+    databaseConfig.setDbSchema("ebean_orm");
+    databaseConfig.setDataSource(this.dataSource);
+    databaseConfig.addClass(FruitRow.class);
+    return DatabaseFactory.create(databaseConfig);
   }
 }
